9cfa4fd800362192e01f0cf0208b7b314b090d29
ashlyn-knox <tristan.knox@edu.sait.ca>
2021-03-26 14:09:01 -0600

    image page for single images

--- routes/api/v0.js
+++ routes/api/v0.js
@@ -18,13 +18,13 @@ router.get('/api/v0/kiraGallery', async (req, res) => {
 
 // single images
 router.get('/:id', async (req, res) => {
-  const id = await Images.find({id: req.params.id}, (err, images) => {
+  const image = await Images.findOne({id: req.params.id})
     try {
       res.render('pages/image')
     } catch (err) {
       res.sendStatus(404)
+      console.log(err)
     }
-  })
 })
 
 module.exports = router;
